742f4589061f57afe1f743ca02443f69e65c93d2
Jean-Luc Ranaivoarivao <ranaivoarivaojl@me.com>
2017-07-05 17:26:36 +0300

    Forgottent commits

--- test/operation.js
+++ test/operation.js
@@ -5,6 +5,18 @@ var Chai = require('chai');
 var expect = Chai.expect;
 
 describe('Operation', function() {
+  
+  describe("Example of markdown without operation", function() {
+    
+    it("should return 'Lorem Ipsum Dolor'", function() {
+      
+      md = `Lorem Ipsum Dolor`;
+      expect( eMd.render( md ) ).to.equal( `Lorem Ipsum Dolor` );
+      
+    });
+    
+  });
+  
   describe("Example of empty inline operation", function() {
     
     it("should return 'Lorem Ipsum Dolor'", function() {
