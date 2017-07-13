0b401540bacfef57fd62c2914ecf5e63078fd34f
Jean-Luc Ranaivoarivao <ranaivoarivaojl@me.com>
2017-07-13 19:44:31 +0300

    ?? if undefined

--- test/operation.js
+++ test/operation.js
@@ -10,55 +10,50 @@ describe('Operation', function() {
 
     it("should return 'Lorem Ipsum Dolor'", function() {
 
-      md = `Lorem Ipsum Dolor`;
-      expect( eMd.render( md ) ).to.equal( `Lorem Ipsum Dolor` );
+      var md = `Lorem Ipsum Dolor`;
+      expect(eMd.render(md)).to.equal(`Lorem Ipsum Dolor`);
 
     });
-
   });
 
   describe("Example of a simple operation", function() {
 
     it("should return 'Lorem Ipsum Dolor 2'", function() {
 
-      md = `Lorem Ipsum Dolor {{1+1}}`;
-      expect( eMd.render( md ) ).to.equal( `Lorem Ipsum Dolor 2` );
+      var md = `Lorem Ipsum Dolor {{1+1}}`;
+      expect(eMd.render(md)).to.equal(`Lorem Ipsum Dolor 2`);
 
     });
-
   });
 
   describe("Example of empty inline operation, spaces in both sides must be erased", function() {
 
     it("should return 'Lorem Ipsum Dolor'", function() {
 
-      md = `Lorem Ipsum {{}} Dolor`;
-      expect( eMd.render( md ) ).to.equal( `Lorem Ipsum Dolor` );
+      var md = `Lorem Ipsum {{}} Dolor`;
+      expect(eMd.render(md)).to.equal(`Lorem Ipsum Dolor`);
 
     });
-
   });
 
   describe("Example of an empty inline operation wraped by styles chars, spaces in both sides must be erased", function() {
 
     it("should return 'Lorem Ipsum Dolor 2'", function() {
 
-      md = `Lorem Ipsum **{{TEST=56+6}}** Dolor`;
-      expect( eMd.render( md ) ).to.equal( `Lorem Ipsum Dolor` );
+      var md = `Lorem Ipsum **{{TEST=56+6}}** Dolor`;
+      expect(eMd.render(md)).to.equal(`Lorem Ipsum Dolor`);
 
     });
-
   });
 
   describe("Example of empty inline operation at a both side of a string", function() {
 
     it("should return 'Lorem Ipsum Dolor'", function() {
 
-      md = `{{TEST=56+6}} Lorem Ipsum Dolor {{TEST=56+6}}`;
-      expect( eMd.render( md ) ).to.equal( `Lorem Ipsum Dolor` );
+      var md = `{{TEST=56+6}} Lorem Ipsum Dolor {{TEST=56+6}}`;
+      expect(eMd.render(md)).to.equal(`Lorem Ipsum Dolor`);
 
     });
-
   });
 
   describe("Example of empty inline operation wraped by styles at a both side of a string", function() {
@@ -66,9 +61,26 @@ describe('Operation', function() {
     it("should return 'Lorem Ipsum Dolor'", function() {
 
       md = `**{{TEST=56+6}}** Lorem Ipsum Dolor **{{TEST=56+6}}**`;
-      expect( eMd.render( md ) ).to.equal( `Lorem Ipsum Dolor` );
+      expect(eMd.render(md)).to.equal(`Lorem Ipsum Dolor`);
+
+    });
+  });
+
+  describe("Example of a undefined variable", function() {
+
+    it("should return 'Lorem Ipsum Dolor 62'", function() {
+
+
 
     });
 
+    it("should return 'Lorem Ipsum Dolor ??' and not 'Lorem Ipsum Dolor 2' and not because scope unique by render", function() {
+      var md1 = `Lorem Ipsum Dolor {{TEST=1+1}}`;
+      var foo = eMd.render(md1);
+
+      var md2 = `Lorem Ipsum Dolor {{TEST}}`;
+      expect(eMd.render(md2)).to.equal(`Lorem Ipsum Dolor ??`);
+
+    });
   });
 });
